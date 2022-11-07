c49275ef3570fe2702d6a923026aeb80bf3e693b
unknown <gabriel10.speed@hotmail.com>
2022-11-07 16:23:00 -0300

    added walletView and did some changes

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,90 +1,118 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import css from './Login.css';
-import backgroundImg from '../../assets/background-login.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import css from "./Login.css";
+import backgroundImg from "../../assets/background-login.png";
 
-var email
-var password
+var email;
+var password;
 
-function handleChange(){
-    email = document.getElementById("email").value
-    password = document.getElementById("password").value
+function handleChange() {
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
 
-    if(email){
-        if (!validateEmail()){
-            generateNotification("Formato de email inválido", "notification-invalid-email", false)
-        }else{
-            clearNotification("notification-invalid-email")
-        }
+  if (email) {
+    if (!validateEmail()) {
+      generateNotification(
+        "Formato de email inválido",
+        "notification-invalid-email",
+        false
+      );
+    } else {
+      clearNotification("notification-invalid-email");
     }
+  }
 
-    if (password){
-        if(!validatePassword()){
-            generateNotification("A senha deve conter no mínimo 8 caracteres letras maiúsculas, minúsculas e pelo menos 1 caracter especial", "notification-invalid-password", false)
-        }else{
-            clearNotification("notification-invalid-password")
-        }
+  if (password) {
+    if (!validatePassword()) {
+      generateNotification(
+        "A senha deve conter no mínimo 8 caracteres letras maiúsculas, minúsculas e pelo menos 1 caracter especial",
+        "notification-invalid-password",
+        false
+      );
+    } else {
+      clearNotification("notification-invalid-password");
     }
+  }
 }
 
-function handleLogin(){
-    if (validateEmail() && validatePassword()){
-        window.location.href = "/wallet"
-    }
+function handleLogin() {
+  if (validateEmail() && validatePassword()) {
+    window.location.href = "/wallet";
+  }
 }
 
-function generateNotification(message, nameElement, delay=true) {
-    document.getElementById(nameElement).innerText = message
-    if(delay){
-        setTimeout(clearNotification, 2 * 1000, nameElement)
-    }
+function generateNotification(message, nameElement, delay = true) {
+  document.getElementById(nameElement).innerText = message;
+  if (delay) {
+    setTimeout(clearNotification, 2 * 1000, nameElement);
+  }
 }
 
 function clearNotification(nameElement) {
-    document.getElementById(nameElement).innerHTML = null
+  document.getElementById(nameElement).innerHTML = null;
 }
 
-function validateEmail(){
-    var regerxEmail =  /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+function validateEmail() {
+  var regerxEmail = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
 
-    if (regerxEmail.test(email)) {
-        return true
-    }else{
-        return false
-    }
+  if (regerxEmail.test(email)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function validatePassword(){
-    var regexPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*\W){8,}/
+function validatePassword() {
+  var regexPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*\W){8,}/;
 
-    if (regexPassword.test(password)) {
-        return true
-    }else{
-        return false
-    }
+  if (regexPassword.test(password)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-window.addEventListener('keyup', handleChange)
+window.addEventListener("keyup", handleChange);
 
 const Login = () => {
-    return (
-        <div className="container" id="container">
-            <div className="form-container sign-in-container">
-                <form>
-                    <h1 className='login-title'>Login para continuar</h1>
-                    <div id="notification-error" className="notificatiob-error"></div>
-                    <input id="email" type="email" placeholder="Email" />
-                    <div className="notification-invalid" id="notification-invalid-email"></div>
-                    <input id="password" type="password" placeholder="Senha" pattern="(?=.*[a-z])(?=.*[A-Z]).{8,}" />
-                    <div className="notification-invalid" id="notification-invalid-password"></div>
-                    <button className="button" type="button" id="login" onClick={handleLogin} >Login</button>
-                </form>
-            </div>
-            <div className="overlay-container">
-                <img className='background-login-image' src={backgroundImg} />
-            </div>
+  return (
+    <section className="login-body">
+      <div className="container" id="container">
+        <div className="form-container sign-in-container">
+          <form>
+            <h1 className="login-title">Login para continuar</h1>
+            <div id="notification-error" className="notificatiob-error"></div>
+            <input id="email" type="email" placeholder="Email" />
+            <div
+              className="notification-invalid"
+              id="notification-invalid-email"
+            ></div>
+            <input
+              id="password"
+              type="password"
+              placeholder="Senha"
+              pattern="(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            />
+            <div
+              className="notification-invalid"
+              id="notification-invalid-password"
+            ></div>
+            <button
+              className="button"
+              type="button"
+              id="login"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </form>
         </div>
-    );
-}
+        <div className="overlay-container">
+          <img className="background-login-image" src={backgroundImg} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
--- src/pages/wallet/Wallet.js
+++ src/pages/wallet/Wallet.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import css from "./Wallet.css";
+import { Link } from "react-router-dom";
+import logoWallet from "../../assets/logo-wallet.png";
+import exitIcon from "../../assets/exit-icon.png";
+import moneyIcon from "../../assets/money-icon.png";
 
-const Wallet = () =>{
+const Wallet = () => {
   return (
-    <div>
-      <h1>Wallet</h1>
-      <nav>
-      </nav>
-    </div>
+    <section className="wallet-body">
+      <div>
+        <header className="header">
+          <nav className="navbar">
+            <a className="logo-wallet" href="">
+              <img src={logoWallet} />
+            </a>
+            <h1>CARTEIRA</h1>
+            <a className="exit-icon" href="">
+              <img src={exitIcon} />
+            </a>
+          </nav>
+        </header>
+
+        <div className="container-activ">
+          <div className="activates">
+            <div className="icon-activates">
+              <img className="icon-img" src={moneyIcon} />
+            </div>
+            <h1 className="text-activates">Total de Despesas</h1>
+
+            <div>
+              <h1 className="text-result">R$ 0,00</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="insert-container-wallet">
+          <div className="buttons">
+            <button className="btn-insert" type="button">
+              Cadastrar Despesa
+            </button>
+          </div>
+        </div>
+        <h1>Wallet</h1>
+      </div>
+    </section>
   );
-}
+};
 
 export default Wallet;
