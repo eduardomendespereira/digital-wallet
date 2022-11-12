3f2db39dab9a3a377aee4264891bd0b950ab13b1
Eduardo Mendes <eduardompereira4@outlook.com>
2022-11-11 23:59:11 -0300

    add coins list

--- src/pages/wallet/Wallet.jsx
+++ src/pages/wallet/Wallet.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import styles from "./Wallet.css";
-import { Link } from "react-router-dom";
 import logoWallet from "../../assets/logo-wallet.png";
 import imageAddExpense from "../../assets/imageAddExpense.png"
 import exitIcon from "../../assets/exit-icon.png";
@@ -8,16 +7,23 @@ import moneyIcon from "../../assets/money-icon.png";
 import ExpenseTable from "../../components/table/ExpenseTable.jsx";
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import {Button} from "@material-ui/core";
+import axios from 'axios'
+
+const coins = () => {
+  axios.get('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => {
+        console.log(response.data)
+      })
+}
 
 const InsertModal = () => {
   const [open, setOpen] = useState(false)
   return (
-    <div className={styles}>
+    <div>
       <button className="btn-insert" type="button" onClick={() => setOpen(true)}>Cadastrar Despesa</button>
       <Modal className="modal" open={open} onClose={() => setOpen(false)}>
         <form className="form-add-expense">
-            <img className="image-add-expense" src={imageAddExpense} />
+          <img className="image-add-expense" src={imageAddExpense} />
           <h1 className="title-insert-form">Cadastrar Despesa</h1>
           <input className="description-input-form-insert" placeholder="Descrição"/>
           <div className="wrap-inputs-form-add-expense">
@@ -26,7 +32,7 @@ const InsertModal = () => {
           </div>
           <input placeholder="Método de Pagamento"/>
           <input placeholder="Tag"/>
-          <button className="btn-submit" type="button">Adicionar Despesa</button>
+          <button className="btn-submit" type="button" onClick={coins}>Adicionar Despesa</button>
         </form>
       </Modal>
     </div>
