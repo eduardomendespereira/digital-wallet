98cc6baef667a7ab43288ccbcdbe6cf1abd83823
Eduardo Mendes <eduardompereira4@outlook.com>
2022-11-17 10:39:01 -0300

    feat: install redux and add store.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css';
-
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Wallet from "./pages/wallet/Wallet.jsx";
 import Login from "./pages/login/Login.jsx";
+import { Provider } from 'react-redux';
+import { store } from './store.js';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/wallet" element={<Wallet />} />
-                <Route path="/" element={<Login />} />
-            </Routes>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/wallet" element={<Wallet />} />
+                    <Route path="/" element={<Login />} />
+                </Routes>
+            </Router>
+        </Provider>
     );
 }
 
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+
+export const store = configureStore({
+    reducer:{
+    }
+    });
