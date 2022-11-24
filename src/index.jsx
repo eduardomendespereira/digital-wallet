e8ff897c09e29198ee6fcded9d8138985bfe8989
Eduardo Mendes <eduardompereira4@outlook.com>
2022-11-24 10:56:33 -0300

    feat: create test index

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import './index.css';
+
+export const ReactStrictMode = <React.StrictMode>
+  <App />
+</React.StrictMode>
+
+export const rootElement = document.getElementById('root')
+
+ReactDOM.render(
+  ReactStrictMode,
+  rootElement
 );
